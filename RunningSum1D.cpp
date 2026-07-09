deff15c7fb20df9bdea81ca534623e13d2f83fd3
Jatin Kumar <jatinkumargdm@gmail.com>
2026-07-09 14:43:01 +0530

    problem 1480

---
+++ RunningSum1D.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+         for (int i = 1; i < nums.size(); i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+
+// 2nd approach using some extra space to store the sum of array in temp 
+
+        //   vector<int> temp; //empty vector
+       
+        // int sum = 0;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     sum += nums[i];
+        //     temp.push_back(sum);
+        // }
+        //  return temp;
+
+
+// 1st approach using brute force in 0(n2) and 0(n)
+
+        //  vector<int> temp;
+
+        // for (int i = 0; i < nums.size(); i++) {
+        //     int sum = 0;
+
+        //     for (int j = 0; j <= i; j++) {
+        //         sum += nums[j];
+        //     }
+
+        //     temp.push_back(sum);
+        // }
+
+        // return temp;
+
+    }
+};
