11e33ea9a27f39fd9347cc344be9cb3691311fee
Jatin Kumar <jatinkumargdm@gmail.com>
2026-07-20 13:22:47 +0530

    Problem 412

---
+++ FizzBuzz.cpp
@@ -0,0 +1,48 @@
+#include<vector>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> answer; //0(n)both
+        // for (int i = 1; i <= n; i++) {
+        //         if (i % 15 == 0)
+        //             answer.push_back("FizzBuzz");
+        //         else if (i % 3 == 0)
+        //             answer.push_back("Fizz");
+        //         else if (i % 5 == 0)
+        //             answer.push_back("Buzz");
+        //         else
+        //             answer.push_back(to_string(i));
+        //     }
+        
+        for (int i = 1; i <= n; i++) {
+            answer.push_back(
+                i % 15 == 0 ? "FizzBuzz" :
+                i % 3 == 0  ? "Fizz" :
+                i % 5 == 0  ? "Buzz" :
+                to_string(i)
+            );
+        }
+            return answer;
+    }
+};
+
+//2nd Using string concatenation 0(n)both
+    // class Solution {
+    // public:
+    //     vector<string> fizzBuzz(int n) {
+    //         vector<string> answer;
+
+    //         for (int i = 1; i <= n; i++) {
+    //             string str = "";
+
+    //             if (i % 3 == 0) str += "Fizz";
+    //             if (i % 5 == 0) str += "Buzz";
+
+    //             answer.push_back(str.empty() ? to_string(i) : str);
+    //         }
+
+    //         return answer;
+    //     }
+    // };
