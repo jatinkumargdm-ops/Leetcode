01a25d92bb3ab1a21331f64abd831c96fc0ee77f
Jatin kumar <jatinkumargdm@gmail.com>
2026-07-21 12:45:36 +0530

    One more approach added to  Problem 412
    Commented out the FizzBuzz implementation without using the modulus operator.

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -46,3 +46,33 @@ public:
     //         return answer;
     //     }
     // };
+
+
+//Without % op
+
+//      vector<string> answer;
+//         int fizz = 0, buzz = 0;
+
+//         for (int i = 1; i <= n; i++) {
+//             fizz++;
+//             buzz++;
+
+//             if (fizz == 3 && buzz == 5) {
+//                 answer.push_back("FizzBuzz");
+//                 fizz = 0;
+//                 buzz = 0;
+//             }
+//             else if (fizz == 3) {
+//                 answer.push_back("Fizz");
+//                 fizz = 0;
+//             }
+//             else if (buzz == 5) {
+//                 answer.push_back("Buzz");
+//                 buzz = 0;
+//             }
+//             else {
+//                 answer.push_back(to_string(i));
+//             }
+//         }
+
+//         return answer;
